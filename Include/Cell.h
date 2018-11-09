a6a0cee96ac88fcdeeec99f7e1e9626a7f9a4b7b
toarukagakunoSirius <harrychienhsu@gmail.com>
2018-11-09 16:20:12 +0000

    update folders

--- Include/Cell.h
+++ Include/Cell.h
@@ -6,38 +6,38 @@ using namespace std;
 class cell {
 protected:
       int CellID, material;
-      char type;
+      string type;
       int* p;
 public:
-      cell(int ID, int m, char type);
-      virtual double getvolume();
-      double getweight(double volume);
-      virtual double getcentre();
+      setCell(int ID, int m, string type);
+      //virtual double getvolume();
+      //double getweight(double volume);
+      //virtual double getcentre();
 }
 
 /*----------------------------------------------------------------------------*/
 
 class tetrahedron : public cell {
 public:
-      tetrahedron(int p[4]);
-      double getvolume();
-      double getcentre();
+      setTetrahedron(int p[4]);
+      //double getvolume();
+      //double getcentre();
 }
 
 /*----------------------------------------------------------------------------*/
 
 class pyramid : public cell {
 public:
-      pyramid(int p[5]);
-      double getvolume();
-      double getcentre();
+      setyPyramid(int p[5]);
+      //double getvolume();
+      //double getcentre();
 }
 
 /*----------------------------------------------------------------------------*/
 
 class hexahedron : public cell {
 public:
-      hexahedron(int p[8]);
-      double getvolume();
-      double getcentre();
+      setHexahedron(int p[8]);
+      //double getvolume();
+      //double getcentre();
 }
