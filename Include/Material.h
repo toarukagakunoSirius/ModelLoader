f37a1a9023bd795ef7daf6770c43a768cc6a2cc1
Tanupatch Kusolvitya <eeytk1@nottingham.ac.uk>
2018-11-09 13:40:22 +0000

    add Material.h

--- Include/Material.h
+++ Include/Material.h
@@ -1,8 +1,21 @@
-#pragma once
+#include <string>
+
+using namespace std;
+
 class Material
 {
+private:
+	int MatID;
+	int Density;
+	string MatName;
+	string Colour;
+
 public:
 	Material();
 	~Material();
+	void setMaterial(int RCMat, int RCDen, string RCCol,string RCNam);
+	int getMatID() const;
+	int getDensity() const;
+	string getName() const;
+	string getColour() const;
 };
-
