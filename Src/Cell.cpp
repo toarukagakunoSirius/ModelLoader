79578e631ef5b1020362bee4d1e717e5ec82919f
toarukagakunoSirius <harrychienhsu@gmail.com>
2018-12-12 15:50:45 +0000

    cell edit

--- Src/Cell.cpp
+++ Src/Cell.cpp
@@ -7,19 +7,11 @@
 
 using namespace std;
 
-
-
-void cell::setCell(int ID, int m, string type){
-
-/*Class cell functions*/
-
-//store cellID, matID and cell type
 void cell :: setCell(int ID, int m, string type){
-
       this->CellID = ID;
       this->material = m;
       this->type = type;
-	  cout << "test";
+	    cout << "test";
 }
 
 void cell :: setVertices(int v){
@@ -43,19 +35,12 @@ double cell :: getcentre() {
 }*/
 
 
-
-void tetrahedron::setTetrahedron(int p[4]) {
-      p = new int [4];
-	  for (int i = 0; i < 4; i++) {
-            p[i] = p[i];
-
 /*Member class tetrahedron functions*/
 //store tetrahedron veteces
-void tetrahedron :: setTetrahedron(int p[4]) {
+/*void tetrahedron :: setTetrahedron(int p[4]) {
       this->p = new int [4];
       for (i = 0, i < 4, i++){
             this->p[i] = p[i];
-
       }
 }
 
@@ -68,19 +53,12 @@ double tetrahedron :: getcentre() {
 }*/
 
 
-
-void pyramid::setPyramid(int p[5]) {
-      p = new int [5];
-	  for (int i = 0; i < 5; i++) {
-            p[i] = p[i];
-
 /*Member class pyramid functions*/
 //store pyramid veteces
-void pyramid :: setPyramid(int p[5]) {
+/*void pyramid :: setPyramid(int p[5]) {
       this->p = new int [5];
       for (i = 0, i < 5, i++){
             this->p[i] = p[i];
-
       }
 }
 
@@ -94,12 +72,9 @@ double pyramid :: getcentre() {
 }*/
 
 
-
-void hexahedron::setHexahedron(int p[8]) {
-
 /*Member class hexahedron functions*/
 //store hexaahedron veteces
-void hexahedron :: setHexahedron(int p[8]) {
+/*void hexahedron :: setHexahedron(int p[8]) {
 
       this->p = new int [8];
 	  for (int i = 0; i < 8; i++) {
