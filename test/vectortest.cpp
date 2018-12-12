c9bfcef2dd11a01b16bc9c94abc5717be406e23e
Hana <eeyhmm@nottingham.ac.uk>
2018-12-12 18:20:50 +0000

    TEST FILE WORKING!

--- test/vectortest.cpp
+++ test/vectortest.cpp
@@ -9,25 +9,36 @@
 using namespace std;
 
 int main() {
+	float t;
 	//x,y,z for vector 1
 	float x1 = 1;
 	float y1 = 2;
 	float z1 = 3;
-	Vector Test1;
+	Vector Test1, Test2, Test3, Test4, Test5;
 	Test1 = Vector(x1, y1, z1);
+	Test3 = Vector(x1, y1, z1);
+	Test4 = Vector(x1, y1, z1);
 
 	//x,y,z for vector 2
 	float x2 = 4;
 	float y2 = 5;
 	float z2 = 6;
-	Vector Test2;
+	
 	Test2 = Vector(x2, y2, z2);
+	
 	Test1.add(Test2);
 	cout << "the sum of vector 1 and vector 2 is:" << endl;
 	Test1.print();
 
-
-	Test1.subtract(Test2); //NOT OUTPUTTING THE CORRECT NUMBER!!!!!!!!!!
+	Test3.subtract(Test2); 
 	cout << "the difference of vector 1 and vector 2 is:" << endl;
-	Test1.print();
+	Test3.print();
+
+	t = Test4.scalarproduct(Test2);
+	cout << "the scalar product of vector 1 and vector 2 is:\n"<< t << endl;
+	
+
+	Test5 = Test1.vectorproduct(Test2);
+	cout << "the vector product of vector 1 and vector 2 is:" << endl;
+	Test5.print();
 }
