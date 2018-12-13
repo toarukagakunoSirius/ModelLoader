973628ea8901926a9ce3cb17db3f4d5ca67ffc99
toarukagakunoSirius <harrychienhsu@gmail.com>
2018-12-13 19:14:58 +0000

    edited cell test program

---
+++ Test_Programs/Cell_Test/Include/Cell.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*Class cell*/
+class cell {
+protected:
+      int CellID, material;
+      string type;
+      vector<int> p;
+public:
+      void setCell(int ID, int m, string type); //store cellID, matID and cell type
+      void setVertices(int v);
+	  string getType();
+	  int getCellID();
+	  int getMaterialID();
+	  int getVerticesID(int n);
+      //virtual double getvolume();
+      //double getweight(double volume);
+      //virtual double getcentre();
+};
+
+
+class Pyramid : public cell {
+	
+};
+
+class Tetrahedron : public cell {
+
+};
+
+class Hexahedron : public cell {
+
+};
+
+
+
+
+/*Member class tetrahedron*/
+/*class tetrahedron : public cell {
+public:
+      void setTetrahedron(int p[4]); //store tetrahedron veteces
+      //double getvolume();
+      //double getcentre();
+};
+
+
+/*Member class pyramid*/
+/*class pyramid : public cell {
+public:
+      void setPyramid(int p[5]);
+      //double getvolume();
+      //double getcentre();
+};
+/*----------------------------------------------------------------------------*/
+/*
+      void setPyramid(int p[5]); //store pyramid veteces
+      //double getvolume();
+      //double getcentre();
+}
+
+/*Menber class hexahedron*/
+/*class hexahedron : public cell {
+public:
+      void setHexahedron(int p[8]); //store hexahedron veteces
+      //double getvolume();
+      //double getcentre();
+}*/
