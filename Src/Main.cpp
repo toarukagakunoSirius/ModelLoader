9382fbd01ebcf491dd2d20f853ee35a34b9aabae
Edward Percy <percy.edward74@gmail.com>
2019-04-08 12:05:12 +0100

    Added doxy comments

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,15 +1,15 @@
 // main.cpp-------------------------------------------------------------------
 
 #include "Main.h"
-
-int main( int argc, char** argv )
-{
-		/** @file
+/** @file
 	* This file contains the declarations of all exported functions.
 	*/
 	/** Brief description
 	* This description will hopefully be picked up on by doxygen
 	*/
+int main( int argc, char** argv )
+{
+		
 	
 
     //Initiate QT stuff
