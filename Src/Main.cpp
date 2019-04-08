3cafc5d38ec7c7a14cdc13a0d0e59607fc94a425
Edward Percy <percy.edward74@gmail.com>
2019-04-08 12:04:44 +0100

    Added doxy comments

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -4,11 +4,14 @@
 
 int main( int argc, char** argv )
 {
-	/*! \brief Brief description.
-	 *         Brief description continued.
-	 *
-	 *  Detailed description starts here.
-	 */
+		/** @file
+	* This file contains the declarations of all exported functions.
+	*/
+	/** Brief description
+	* This description will hopefully be picked up on by doxygen
+	*/
+	
+
     //Initiate QT stuff
 
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
