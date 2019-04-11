f8a4f5604a98f9f439583bfb5e31cd2f6ca34712
Edward Percy <percy.edward74@gmail.com>
2019-04-11 10:49:12 +0100

    Fixing case sensitivity

---
+++ src/vector.cpp
@@ -0,0 +1,109 @@
+﻿//
+//  vector.cpp
+//  vector
+//
+//  Created by Hana Makhlouf on 11/9/18.
+//  Copyright © 2018 Hana Makhlouf. All rights reserved.
+//
+
+#include <iostream>
+#include "vector.h"
+#include <vector> 
+using namespace std;
+
+/*-- -------------------------------------------------------*/
+Vector::Vector( float x, float y, float z ) { //constructor
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+void Vector::SetVector(int iD, float x, float y, float z) { //setting vector values using this
+	this->ID = iD;
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+Vector::Vector() { //default constructor just in case no values are entered
+    this->x = 0.;
+    this->y = 0.;
+    this->z = 0.;
+}
+
+/*---------------------------------------------------------*/
+//add function
+void Vector::add( Vector v ) {
+    this->x += v.x;
+    this->y += v.y;
+    this->z += v.z;
+}
+
+
+Vector Vector::divide(int n) { //divide function
+	Vector vector1;
+	vector1.x = this->x / n;
+	vector1.y = this->y / n;
+	vector1.z = this->z / n;
+	
+	return vector1;
+}
+
+//add function using operator overloading
+Vector Vector::operator+(Vector v) { //this allows us to use + to add 2 vectors instead of using the add function above
+	Vector vector1;
+	vector1.x = this->x + v.x;
+	vector1.y = this->y + v.y;
+	vector1.z = this->z + v.z;
+	return vector1;
+}
+
+/*---------------------------------------------------------*/
+
+//subtract function
+void Vector::subtract( Vector v ){
+    this->x -= v.x;
+    this->y -= v.y;
+    this->z -= v.z;
+}
+
+//subtract function using operator overloading 
+Vector Vector::operator-(Vector v) {
+	Vector vector1;
+	vector1.x = this->x - v.x;
+	vector1.y = this->y - v.y;
+	vector1.z = this->z - v.z;
+	return vector1;
+}
+
+/*---------------------------------------------------------*/
+//scalar product function
+float Vector::scalarproduct( Vector v ){ //dot product
+    float scalarresult = this->x*v.x + this->y*v.y + this->z*v.z;
+    return scalarresult;
+}
+
+//vector product function
+Vector  Vector:: vectorproduct( Vector v){ //cross product
+    float vectorresult1 = this->y*v.z - this->z*v.y;
+    float vectorresult2 = this->z*v.x - this->x*v.z;
+    float vectorresult3 = this->x*v.y - this->y*v.x;
+    
+    Vector final(vectorresult1,vectorresult2,vectorresult3);
+    
+    return final;
+}
+/*---------------------------------------------------------*/
+//print function - prints vectors
+void Vector::print() {
+
+	cout << this-> x << "," << this-> y << "," << this-> z << endl;
+
+}
+
+float Vector::getx(){ return x; }
+float Vector::gety(){ return y; }
+float Vector::getz(){ return z; }
+int Vector::getID() { return ID; }
+
+/*---------------------------------------------------------*/
